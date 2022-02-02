8b909c84b576dfbac8db3618faae6857d05a60c8
Luke <lslee714@gmail.com>
2022-02-01 21:50:18 -0700

    fix bubble sort -.-

--- tinker/algorithms/bubble_sort.py
+++ tinker/algorithms/bubble_sort.py
@@ -1,13 +1,10 @@
-def bubble_sort(aList):
-    for idx, element in enumerate(aList):
-        swapped = False
-        start = idx + 1
-        while start < len(aList):
-            next_element = aList[start]
-            if element > next_element:
-                aList[idx], aList[start] = aList[start], aList[idx]
-                swapped = True
-            start += 1
-        if not swapped:
-            break
-    return aList
+def bubble_sort(a_list_to_sort):
+    for i in range(len(a_list_to_sort)):
+        for j in range(len(a_list_to_sort) - i):
+            elem = a_list_to_sort[j]
+            next_idx = j+1 if j < len(a_list_to_sort) - 1 else None
+            next_elem = a_list_to_sort[next_idx] if next_idx else None
+
+            if next_elem and elem > next_elem:
+                a_list_to_sort[j], a_list_to_sort[next_idx] = a_list_to_sort[next_idx], a_list_to_sort[j]
+    return a_list_to_sort
