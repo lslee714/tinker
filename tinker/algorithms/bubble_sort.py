5c9697f6065be3759221e564c696c20993024c76
Luke <lslee714@gmail.com>
2022-03-01 20:05:16 -0700

    just another commit

--- tinker/algorithms/bubble_sort.py
+++ tinker/algorithms/bubble_sort.py
@@ -8,3 +8,4 @@ def bubble_sort(a_list_to_sort):
             if next_elem and elem > next_elem:
                 a_list_to_sort[j], a_list_to_sort[next_idx] = a_list_to_sort[next_idx], a_list_to_sort[j]
     return a_list_to_sort
+
